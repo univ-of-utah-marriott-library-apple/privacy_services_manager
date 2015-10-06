25024a73d82e097390f672a874e6ff8d7e2e77af
Pierce Darragh <pierce.darragh@utah.edu>
2015-10-06 11:49:02 -0600

    Updates to the administrative override
    Modified the administrative override to be able to add application
    bundles, instead of just binaries. This allows bundle identifiers (e.g.
    `com.mycompany.coolapp`) to be added without the application being
    looked up. Useful for systems administrators.
    
    The old `—admin` flag still exists and will map correctly to
    `—no-check-bin`, so don’t worry about old code breaking. :)

--- privacy_services_management/universal.py
+++ privacy_services_management/universal.py
@@ -5,14 +5,14 @@ import tcc_services
 attributes = {
     'long_name': "Privacy Services Manager",
     'name':      "privacy_services_manager",
-    'version':   "1.6.11",
+    'version':   "1.7.0",
 }
 
 # This is a list of services which can be modified.
 # Useful for scripts to call on for a neat list.
 available_services = tcc_services.available_services.keys() + ['location']
 
-def get_editor(service, logger, user='', template=False, lang='English', forceroot=False, admin=False):
+def get_editor(service, logger, user='', template=False, lang='English', forceroot=False, no_check=False, no_check_type=None):
     """
     Returns the appropriate type of editor for the given service. This allows
     for a more generalized approach in other scripts, as opposed to having to
@@ -26,14 +26,19 @@ def get_editor(service, logger, user='', template=False, lang='English', forcero
         if service in tcc_services.available_services.keys():
             # If it's in the TCC services, return a pre-formatted one of those.
             return tcc_services.TCCEdit(
-                service   = service,
-                user      = user,
-                template  = template,
-                lang      = lang,
-                logger    = logger,
-                forceroot = forceroot,
-                admin     = admin,
+                service         = service,
+                user            = user,
+                template        = template,
+                lang            = lang,
+                logger          = logger,
+                forceroot       = forceroot,
+                no_check        = no_check,
+                no_check_type   = no_check_type
             )
         else:
             # Otherwise, return an editor for Location Services.
-            return location_services.LSEdit(logger=logger, admin=admin)
+            return location_services.LSEdit(
+                logger          = logger,
+                no_check        = no_check,
+                no_check_type   = no_check_type
+            )
